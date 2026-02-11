47fb071eab58cfb25d5e3a07ae6bc0ad2895c328
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-02-10 22:05:39 -0500

    Add board config files (#7)

---
+++ Catan_Diagram/MapLayout.java
@@ -0,0 +1,15 @@
+package Catan_Diagram;
+
+public class MapLayout {
+    public static final int [] tokens = {0, 4, 3, 11, 6, 4, 3, 11, 6, 5, 8, 9, 12, 10, 2, 9, 10, 8, 5};
+    public static final ResourceType [] tileResource = {
+        ResourceType.DESERT, ResourceType.GRAIN, ResourceType.ORE, 
+        ResourceType.LUMBER, ResourceType.BRICK, ResourceType.WOOL,
+        ResourceType.LUMBER, ResourceType.WOOL, ResourceType.GRAIN,
+        ResourceType.BRICK, ResourceType.LUMBER, ResourceType.GRAIN,
+        ResourceType.GRAIN, ResourceType.ORE, ResourceType.WOOL, 
+        ResourceType.LUMBER, ResourceType.BRICK, ResourceType.ORE,
+        ResourceType.WOOL
+    };
+    
+}
